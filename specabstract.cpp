620c1b491e024b12fa97226e2b90fb631f1ed1cd
horsicq <horsicq@gmail.com>
2020-05-19 00:10:17 +0200

    Fix

--- specabstract.cpp
+++ specabstract.cpp
@@ -4484,6 +4484,21 @@ void SpecAbstract::PE_handle_NETProtection(QIODevice *pDevice,bool bIsImage, Spe
                     pPEInfo->mapResultNETObfuscators.insert(ss.name,scansToScan(&(pPEInfo->basic_info),&ss));
                 }
             }
+            // ReNET-Pack
+            if(pe.checkOffsetSize(pPEInfo->osCodeSection)&&(pPEInfo->basic_info.bIsDeepScan))
+            {
+                qint64 _nOffset=pPEInfo->osCodeSection.nOffset;
+                qint64 _nSize=pPEInfo->osCodeSection.nSize;
+
+                qint64 nOffset_detect=pe.find_signature(&(pPEInfo->basic_info.memoryMap),_nOffset,_nSize,"'Protected/Packed with ReNET-Pack by stx'");
+
+                if(nOffset_detect!=-1)
+                {
+                    _SCANS_STRUCT ss=getScansStruct(0,RECORD_FILETYPE_PE,RECORD_TYPE_NETCOMPRESSOR,RECORD_NAME_RENETPACK,"2.0-3.X","",0);
+
+                    pPEInfo->mapResultNETCompressors.insert(ss.name,scansToScan(&(pPEInfo->basic_info),&ss));
+                }
+            }
         }
 
         // Xenocode Virtual Application Studio 2009
