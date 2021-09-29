1f934866c259b163039465f4067e694e1a2a8490
horsicq <horsicq@gmail.com>
2021-09-29 06:40:57 +0200

    Fix: 2021-09-29

--- specabstract.cpp
+++ specabstract.cpp
@@ -17290,6 +17290,7 @@ SpecAbstract::_SCANS_STRUCT SpecAbstract::getScansStructFromOsInfo(XBinary::OSIN
     else if (osinfo.osType==XBinary::OSTYPE_XBOX)       result.name=RECORD_NAME_XBOX;
     else if (osinfo.osType==XBinary::OSTYPE_OS2)        result.name=RECORD_NAME_OS2;
     else if (osinfo.osType==XBinary::OSTYPE_OSX)        result.name=RECORD_NAME_OSX;
+    else if (osinfo.osType==XBinary::OSTYPE_ANDROID)    result.name=RECORD_NAME_ANDROID;
 
     result.sVersion=osinfo.sOsVersion;
     result.sInfo=QString("%1, %2, %3").arg(osinfo.sArch,XBinary::modeIdToString(osinfo.mode),osinfo.sType);
