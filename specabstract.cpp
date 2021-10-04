6cdfddcf9d93ae8137c7bdbbb4107f9773b9b1c6
horsicq <horsicq@gmail.com>
2021-10-04 18:11:27 +0200

    Fix: 2021-10-04

--- specabstract.cpp
+++ specabstract.cpp
@@ -17292,6 +17292,7 @@ SpecAbstract::_SCANS_STRUCT SpecAbstract::getScansStructFromOsInfo(XBinary::OSIN
     else if (osinfo.osName==XBinary::OSNAME_TRU64)      result.name=RECORD_NAME_TRU64;
     else if (osinfo.osName==XBinary::OSNAME_MODESTO)    result.name=RECORD_NAME_MODESTO;
     else if (osinfo.osName==XBinary::OSNAME_OPENVMS)    result.name=RECORD_NAME_OPENVMS;
+    else if (osinfo.osName==XBinary::OSNAME_FENIXOS)    result.name=RECORD_NAME_FENIXOS;
 
     result.sVersion=osinfo.sOsVersion;
     result.sInfo=QString("%1, %2, %3").arg(osinfo.sArch,XBinary::modeIdToString(osinfo.mode),osinfo.sType);
