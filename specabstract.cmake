6c5f8bc7b36c822290cbc3dfe8659b65246b37f0
horsicq <horsicq@gmail.com>
2021-11-30 18:36:35 +0100

    Fix: 2021-11-30

--- specabstract.cmake
+++ specabstract.cmake
@@ -1,7 +1,8 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# TODO
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 
 set(SPECABSTRACT_SOURCES
+    ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/specabstract.cpp
 )
