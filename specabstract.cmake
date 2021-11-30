f0b0f395fbb901af4ae6deab44584138451c116c
horsicq <horsicq@gmail.com>
2021-11-30 19:04:01 +0100

    Fix: 2021-11-30

--- specabstract.cmake
+++ specabstract.cmake
@@ -2,9 +2,11 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../XDEX/xdex.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../XArchive/xarchives.cmake)
 
 set(SPECABSTRACT_SOURCES
     ${XFORMATS_SOURCES}
     ${XDEX_SOURCES}
+    ${XARCHIVES_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/specabstract.cpp
 )
