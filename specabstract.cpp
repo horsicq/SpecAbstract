05cf6ab7837a11dd0a027ccd6d492cfe77042116
horsicq <horsicq@gmail.com>
2022-03-01 19:16:52 +0100

    Fix: 2022-03-01

--- specabstract.cpp
+++ specabstract.cpp
@@ -8405,7 +8405,7 @@ void SpecAbstract::PE_handle_PETools(QIODevice *pDevice, bool bIsImage, SpecAbst
     }
 }
 
-void SpecAbstract::PE_handle_wxWidgets(QIODevice *pDevice, bool bIsImage, SpecAbstract::PEINFO_STRUCT *pPEInfo)
+void SpecAbstract::PE_handle_wxWidgets(QIODevice *pDevice,bool bIsImage,SpecAbstract::PEINFO_STRUCT *pPEInfo)
 {
     XPE pe(pDevice,bIsImage);
 
