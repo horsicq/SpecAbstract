45d262fd2ada7165052cf8b181cdada739c41376
horsicq <horsicq@gmail.com>
2022-03-23 08:59:37 +0100

    Fix: 2022-03-23

--- specabstract.cpp
+++ specabstract.cpp
@@ -11715,7 +11715,7 @@ void SpecAbstract::Binary_handle_ProtectorData(QIODevice *pDevice,bool bIsImage,
     }
 }
 
-void SpecAbstract::Binary_handle_LibraryData(QIODevice *pDevice, bool bIsImage, SpecAbstract::BINARYINFO_STRUCT *pBinaryInfo)
+void SpecAbstract::Binary_handle_LibraryData(QIODevice *pDevice,bool bIsImage,SpecAbstract::BINARYINFO_STRUCT *pBinaryInfo)
 {
     XBinary binary(pDevice,bIsImage);
 
