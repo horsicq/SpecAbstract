81ee76564a91cb3c90c4c802fdee0549ef7d9dd4
horsicq <horsicq@gmail.com>
2023-06-19 00:11:39 +0200

    Fix: 2023-06-19

--- specabstract.cpp
+++ specabstract.cpp
@@ -3400,7 +3400,7 @@ SpecAbstract::VI_STRUCT SpecAbstract::_get_ARMC_string(QString sString)
     return result;
 }
 
-SpecAbstract::VI_STRUCT SpecAbstract::_get_ARMCCPP_string(QString sString)
+SpecAbstract::VI_STRUCT SpecAbstract::_get_ARMCCPP_string(const QString &sString)
 {
     VI_STRUCT result = {};
 
