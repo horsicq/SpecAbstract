28b856cefff3764b9d5628c629b78de07bb5e4a1
horsicq <horsicq@gmail.com>
2024-02-28 19:07:25 +0100

    Update file(s): 2024-02-28

--- staticscan.cmake
+++ staticscan.cmake
@@ -5,4 +5,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/../SpecAbstract/specabstract.cmake)
 set(STATICSCAN_SOURCES
     ${SPECABSTRACT_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/staticscan.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/staticscan.h
 )
