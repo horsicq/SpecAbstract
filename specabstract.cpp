f4e28af132e342d31ff4364aaed38bcb5d57fcb3
horsicq <horsicq@gmail.com>
2024-06-07 00:46:24 +0200

    Update file(s): 2024-06-07

--- specabstract.cpp
+++ specabstract.cpp
@@ -12775,6 +12775,10 @@ void SpecAbstract::ELF_handle_Tools(QIODevice *pDevice, SpecAbstract::SCAN_OPTIO
             _SCANS_STRUCT recordSS = getScansStruct(0, XBinary::FT_ELF, RECORD_TYPE_LIBRARY, RECORD_NAME_QT, "5.X", "", 0);
 
             pELFInfo->mapResultLibraries.insert(recordSS.name, scansToScan(&(pELFInfo->basic_info), &recordSS));
+        } else if (XBinary::isStringInListPresent(&(pELFInfo->listLibraries), "libQt6Core_x86.so", pPdStruct)) {
+            _SCANS_STRUCT recordSS = getScansStruct(0, XBinary::FT_ELF, RECORD_TYPE_LIBRARY, RECORD_NAME_QT, "6.X", "", 0);
+
+            pELFInfo->mapResultLibraries.insert(recordSS.name, scansToScan(&(pELFInfo->basic_info), &recordSS));
         }
 
         if (XELF::isNotePresent(&(pELFInfo->listNotes), "Android")) {
