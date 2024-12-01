518e254787adc6ddf23dbbc88d753da84f3f3a68
horsicq <horsicq@gmail.com>
2024-12-02 00:25:20 +0100

    Update file(s): 2024-12-02

--- specabstract.cmake
+++ specabstract.cmake
@@ -1,8 +1,13 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../XScanEngine/xscanengine.cmake)
+if (NOT DEFINED XSCANENGINE_SOURCES)
+    include(${CMAKE_CURRENT_LIST_DIR}/../XScanEngine/xscanengine.cmake)
+    set(SPECABSTRACT_SOURCES ${SPECABSTRACT_SOURCES} ${XSCANENGINE_SOURCES})
+endif()
+
 # TODO Check includes
 set(SPECABSTRACT_SOURCES
+    ${SPECABSTRACT_SOURCES}
     ${XSCANENGINE_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/specabstract.cpp
     ${CMAKE_CURRENT_LIST_DIR}/specabstract.h
