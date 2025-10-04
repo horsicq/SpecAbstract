93824611628d4ca4adc995175ff294d7b5d1b75e
horsicq <horsicq@gmail.com>
2025-10-04 02:35:57 +0200

    Add new file(s): 2025-10-04

---
+++ modules/nfd_atarist.cpp
@@ -0,0 +1,56 @@
+/* Copyright (c) 2019-2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "nfd_atarist.h"
+
+NFD_AtariST::NFD_AtariST(XAtariST *pAtariST, XBinary::FILEPART filePart, OPTIONS *pOptions, XBinary::PDSTRUCT *pPdStruct)
+    : AtariST_Script(pAtariST, filePart, pOptions, pPdStruct)
+{
+}
+
+NFD_AtariST::ATARISTINFO_STRUCT NFD_AtariST::getInfo(QIODevice *pDevice, XScanEngine::SCANID parentId, XScanEngine::SCAN_OPTIONS *pOptions, qint64 nOffset,
+                                                     XBinary::PDSTRUCT *pPdStruct)
+{
+    QElapsedTimer timer;
+    timer.start();
+
+    ATARISTINFO_STRUCT result = {};
+
+    XAtariST atariST(pDevice);
+
+    if (atariST.isValid(pPdStruct) && XBinary::isPdStructNotCanceled(pPdStruct)) {
+        // Initialize BASIC_INFO via shared utility
+        result.basic_info = NFD_Binary::_initBasicInfo(&atariST, parentId, pOptions, nOffset, pPdStruct);
+
+        // Delegate OS detection to NFD
+        Binary_Script::OPTIONS opts = NFD_Binary::toOptions(pOptions);
+
+        NFD_Binary::SCANS_STRUCT ssOperationSystem = NFD_Binary::detectOperationSystem(&atariST, pPdStruct);
+        // Convert and store via shared utility
+        result.basic_info.mapResultOperationSystems.insert(ssOperationSystem.name, NFD_Binary::scansToScan(&(result.basic_info), &ssOperationSystem));
+
+        // Aggregate and finalize result lists
+        NFD_Binary::_handleResult(&(result.basic_info), pPdStruct);
+    }
+
+    result.basic_info.nElapsedTime = timer.elapsed();
+
+    return result;
+}
---
+++ modules/nfd_atarist.h
@@ -0,0 +1,43 @@
+/* Copyright (c) 2019-2025 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef NFD_ATARIST_H
+#define NFD_ATARIST_H
+
+#include "atarist_script.h"
+#include "nfd_binary.h"
+
+class QIODevice;
+
+class NFD_AtariST : public AtariST_Script {
+    Q_OBJECT
+
+public:
+    explicit NFD_AtariST(XAtariST *pAtariST, XBinary::FILEPART filePart, OPTIONS *pOptions, XBinary::PDSTRUCT *pPdStruct);
+
+    struct ATARISTINFO_STRUCT {
+        NFD_Binary::BASIC_INFO basic_info;
+    };
+
+    static ATARISTINFO_STRUCT getInfo(QIODevice *pDevice, XScanEngine::SCANID parentId, XScanEngine::SCAN_OPTIONS *pOptions, qint64 nOffset,
+                                      XBinary::PDSTRUCT *pPdStruct);
+};
+
+#endif  // NFD_ATARIST_H
