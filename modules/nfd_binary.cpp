a5416df3f700138cdbd05b71a9f4375c8c423939
horsicq <horsicq@gmail.com>
2026-03-27 19:07:07 +0100

    refactor(nfd_binary): remove redundant type conversion logic

--- modules/nfd_binary.cpp
+++ modules/nfd_binary.cpp
@@ -3474,10 +3474,8 @@ QList<XScanEngine::SCANSTRUCT> NFD_Binary::convert(QList<SCAN_STRUCT> *pListScan
         record.sVersion = pListScanStructs->at(i).sVersion;
         record.sInfo = pListScanStructs->at(i).sInfo;
 
-        record.globalColorRecord = XScanEngine::typeToColorRecord(record.sType);
         record.nPrio = XScanEngine::typeToPrio(record.sType);
         record.bIsProtection = XScanEngine::isProtection(record.sType);
-        record.sType = XScanEngine::translateType(record.sType);
 
         listResult.append(record);
     }
